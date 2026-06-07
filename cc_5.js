7e1076e8a265e48ae8bedc47e179ed01a961fa2f
Giana Cresta <crestagiana@gmail.com>
2026-06-07 01:11:30 -0400

    pay function

--- cc_5.js
+++ cc_5.js
@@ -4,3 +4,9 @@ const employees = [
     {name: "Jones", hourlyRate: 20, hoursWorked: 45},
     {name: "Lola", hourlyRate: 27, hoursWorked: 48}
 ];
+
+// Pay Function
+function calculateBasePay(rate, hours) {
+    const baseHours = Math.min(hours, 40); // chooses the smaller number (won't go over 40 hrs)
+    return baseHours * rate;
+}
