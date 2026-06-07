ef36b8bb2df42feaef648d28430594b20049424b
Giana Cresta <crestagiana@gmail.com>
2026-06-07 01:04:30 -0400

    employee array

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+// Employee Array
+const employees = [
+    {name: "Kira", hourlyRate: 15, hoursWorked: 24},
+    {name: "Jones", hourlyRate: 20, hoursWorked: 45},
+    {name: "Lola", hourlyRate: 27, hoursWorked: 48}
+];
